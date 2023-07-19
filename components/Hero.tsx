499ca5a049403a5f714fd66499bc09462a6cbb81
Visinho <Elvisvisinho@rocketmail.com>
2023-07-19 17:58:28 +0100

    Completed the footer section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,8 +32,8 @@ const Hero = () => {
         <div className="hero__image-container">
             <div className="hero__image">
                 <Image src="/hero.png" alt="Visinho" fill className="object-contain"/>
-                <div className="hero__image-overlay"></div>
             </div>
+                <div className="hero__image-overlay" />
         </div>
     </div>
   )

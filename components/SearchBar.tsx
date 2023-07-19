e0d916573715d1b1ecd12ea606dd41ecc25da0f1
Visinho <Elvisvisinho@rocketmail.com>
2023-07-19 19:16:46 +0100

    WIP Searchbar components

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+"use client";
+
+import React from 'react';
+import SearchManufacturer from './SearchManufacturer';
+import { useState } from 'react';
+
+// import { SearchManufacturer } from "./"; Also works if the two files are in the same folder and we used index to export it
 
 const SearchBar = () => {
+    const [manufacturer, setManufacturer] = useState("");
+
+    const handleSearch = () => {
+
+    }
+
   return (
-    <div>
-      SearchBar
-    </div>
+    <form className='searchbar' onClick={handleSearch}>
+      <div className='searchbar__item'>
+        {/* Implementing combo box */}
+        <SearchManufacturer 
+            manufacturer={manufacturer}
+            setManufacturer={setManufacturer}
+        />
+      </div>
+    </form>
   )
 }
 

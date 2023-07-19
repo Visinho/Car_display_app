010efcd0c7d390aa2dd766c06012d2179b09ba5c
Visinho <Elvisvisinho@rocketmail.com>
2023-07-19 09:57:01 +0100

    Designed the front page with the image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,6 +28,13 @@ const Hero = () => {
                 handleClick={handleScroll}
             />
         </div>
+
+        <div className="hero__image-container">
+            <div className="hero__image">
+                <Image src="/hero.png" alt="Visinho" fill className="object-contain"/>
+                <div className="hero__image-overlay"></div>
+            </div>
+        </div>
     </div>
   )
 }

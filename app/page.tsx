fa69740a667aa50747c8ddbaf6e2f75e05939901
Visinho <Elvisvisinho@rocketmail.com>
2023-07-19 18:52:04 +0100

    Created and rendered the searchbar and filter components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Hero } from '@/components';
+import { Hero, SearchBar, CustomFilter } from '@/components';
 import Image from 'next/image'
 
 
@@ -8,6 +8,24 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>
+              Car Catalogue
+          </h1>
+          <p>Explore your dream cars</p>
+        </div>
+
+        <div className='home__filters'>
+            <SearchBar />
+            <div className='home__filter-container'>
+              <CustomFilter title="fuel" />
+              <CustomFilter title="year" />
+            </div>
+        </div>
+
+      </div>
     </main>
   )
 }
